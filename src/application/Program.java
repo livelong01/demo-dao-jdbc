d31aae253517026c1352cf4cddfe4c16b46e9221
jonathan alonso <jonathanalonsom@outlook.com>
2023-09-22 15:02:09 +0100

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,19 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Department obj = new Department(1, "books");
+		
+		Seller seller = new Seller(21,"bob", "bob@gmail.com", new Date(), 3000.0, obj);
 
-		System.out.println(obj);
+		System.out.println(seller);
 	}
 
 }
